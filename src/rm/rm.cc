6afe82cde904b55c91d0eff7ff8c93a23ea2bffa
lannywong2000 <lanny_wong@foxmail.com>
2023-02-08 11:46:17 -0800

    debug test

--- src/rm/rm.cc
+++ src/rm/rm.cc
@@ -193,7 +193,7 @@ namespace PeterDB {
         RC rc = rm_ScanIterator.getNextTuple(rid, data);
         std::memcpy(&tableId, data + 1, sizeof(int));
         rm_ScanIterator.close();
-        if (rc != 0) return rc;
+        if (rc != 0) return 100;
         deleteFromSystemFiles(tablesName, rid);
 
         std::vector<RID> toBeDeleted;
