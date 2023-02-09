79bad34e6eb240e341b5aa86fa710dc7bedb523b
lannywong2000 <lanny_wong@foxmail.com>
2023-02-09 00:40:33 -0800

    debug test

--- src/rm/rm.cc
+++ src/rm/rm.cc
@@ -212,6 +212,7 @@ namespace PeterDB {
     RC RelationManager::getAttributes(const std::string &tableName, std::vector<Attribute> &attrs) {
         if (!checkTableExists(tableName)) return ERR_TABLE_NOT_EXISTS;
         int tableId = getTableId(tableName);
+        std::cout << tableId << std::endl;
         RM_ScanIterator rm_ScanIterator;
         scan(columnsName, "table-id", EQ_OP, &tableId, getAttributeAttrs(), rm_ScanIterator);
 
